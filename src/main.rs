f0d023c0d98a077bd11737e26c09ce410cd1753d
AvhiMaz <avhimazumder5@outlook.com>
2026-01-07 20:55:51 +0530

    chore: add handle connection function
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0u8; 4096];
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(0) => {
+                println!("[CLOSE] connection closed");
+                break;
+            }
+            Ok(n) => {
+                println!("[READ] {} bytes", n);
+            }
+            Err(e) => {
+                eprintln!("[ERROR] read failed: {}", e);
+                break;
+            }
+        }
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
