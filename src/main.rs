1f8a1b5a02d70d17ac7f2cf7f9e6302db2c4e754
AvhiMaz <avhimazumder5@outlook.com>
2026-01-11 13:25:32 +0530

    fix: fix tokio impl
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,35 @@
-use std::io::Read;
-use std::net::{TcpListener, TcpStream};
+use tokio::io::AsyncReadExt;
+use tokio::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0u8; 4096];
-
-    tokio::spawn(async move {
-        loop {
-            match stream.read(&mut buf) {
-                Ok(0) => {
-                    println!("[CLOSE] connection closed");
-                    break;
-                }
-                Ok(n) => {
-                    println!("[READ] {} bytes", n);
-                }
-                Err(e) => {
-                    eprintln!("[error] read failed: {}", e);
-                    break;
-                }
+    loop {
+        match stream.read(&mut buf).await {
+            Ok(0) => {
+                println!("[CLOSE] connection closed");
+                break;
+            }
+            Ok(n) => {
+                println!("[READ] {} bytes", n);
+            }
+            Err(e) => {
+                eprintln!("[error] read failed: {}", e);
+                break;
             }
         }
-    });
+    }
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream);
-            }
-            Err(e) => {
-                eprintln!("[error] read failed: {}", e);
-            }
-        }
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        println!("[ACCEPT]: {:?}", addr);
+
+        tokio::spawn(async move {
+            handle_connection(stream).await;
+        });
     }
-
-    println!("listener: {:?}", listener);
-
-    Ok(())
 }
