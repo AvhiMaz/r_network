42276381864163ae74cb6d4e97be7761a24d6225
AvhiMaz <avhimazumder5@outlook.com>
2026-01-11 13:00:07 +0530

    chore: add tokio
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 
-pub fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0u8; 4096];
 
-    loop {
-        match stream.read(&mut buf) {
-            Ok(0) => {
-                println!("[CLOSE] connection closed");
-                break;
-            }
-            Ok(n) => {
-                println!("[READ] {} bytes", n);
-            }
-            Err(e) => {
-                eprintln!("[error] read failed: {}", e);
-                break;
+    tokio::spawn(async move {
+        loop {
+            match stream.read(&mut buf) {
+                Ok(0) => {
+                    println!("[CLOSE] connection closed");
+                    break;
+                }
+                Ok(n) => {
+                    println!("[READ] {} bytes", n);
+                }
+                Err(e) => {
+                    eprintln!("[error] read failed: {}", e);
+                    break;
+                }
             }
         }
-    }
+    });
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
 
     for stream in listener.incoming() {
